43d6f5e5a8b1d9803fb92e790076c33fbd2c342b
jounilammi <jjj.lammi@gmail.com>
2017-11-24 02:01:36 +0200

    Add files via upload

--- RandomListCompare.py
+++ RandomListCompare.py
@@ -8,7 +8,7 @@ def compareAndPrint(number, top):
         list1 = [None] * number
         list2 = [None] * number
 
-        start_time = time.time()
+        
         for i in range(len(list1)):
             list1[i] = random.randint(0,top)
         
@@ -16,26 +16,27 @@ def compareAndPrint(number, top):
         print("List made first:         "," ".join(map(str,list1)))
         k = 0
         i = 0
+        start_time = time.time()
         while(True):
             
             while(list2[k] != list1[k]):
                 list2[k] = random.randint(0,top)
                 i += 1
-                print("Second list is currently "," ".join(map(str,list2)), end = '\r')
+                print("Second list is currently  {}                  ".format(" ".join(map(str,list2))), end = '\r')
             k += 1
             if(list2 == list1):
-                print("Second list is currently "," ".join(map(str,list2)), end = '\r')
                 print("")
+                print("")
+                print("Second list is currently  {}                  ".format(" ".join(map(str,list2))), end = '\r')
                 break
         end_time = time.time()
         total_time = round(end_time - start_time, 6)
         break
     print("Randomly generating the second list to match the first list took {} seconds and {} steps".format(total_time,i))
     print("")
-    print("")
-    print("")
-    print("")
-    print("")
+   
+   
+    
 
     
     
@@ -45,11 +46,12 @@ def onlyCompare(number, top):
         list1 = [None] * number
         list2 = [None] * number
 
-        start_time = time.time()
+        
         for i in range(len(list1)):
             list1[i] = random.randint(0,top)
         k = 0
         i = 0
+        start_time = time.time()
         while(True):
             while(list2[k] != list1[k]):
                 list2[k] = random.randint(0,top)
@@ -65,9 +67,7 @@ def onlyCompare(number, top):
     print("Randomly generating the second list to match the first list took {} seconds and {} steps".format(total_time,i))
     print("")
     print("")
-    print("")
-    print("")
-    print("")
+   
     
 if __name__ == "__main__":
     print("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
@@ -93,7 +93,7 @@ if __name__ == "__main__":
             
         else:
             print("Do you want the program to print the lists on each try? This makes the operation much slower.")
-            print("Note that if the list is linger than your screen is wide, the printing will start to fill the console.")
+            print("Note that if the list is longer than your screen is wide, the printing will start to fill the console.")
             while True:
                 printing = input("Your choice (y/n):")
                 print("")
